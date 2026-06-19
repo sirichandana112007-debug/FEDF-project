5c349666aba520dcbeab7f3ce1d4b9bb20ad15e1
sirichandana112007-debug <sirichandana112007@gmail.com>
2026-06-19 09:50:21 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./App.css";
-import { TourismProvider } from "./TourismContext";
+import App from "./App.jsx";
+import { TourismProvider } from "./TourismContext.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
